8936bcbb28a14f3c09f78c880699a86de0df983b
javacake93 <javacake93@gmail.com>
2024-12-27 09:24:30 +0900

    Update CategoryListModal.js

--- frontend/src/sapa/CategoryListModal.js
+++ frontend/src/sapa/CategoryListModal.js
@@ -5,205 +5,224 @@ import icon_x_white from './../assets/images/icon_x_white.svg';
 import axios from 'axios';
 import ReactJsPagination from "react-js-pagination";
 import { CategoryContext, useCategoryContext } from '../CategoryContexts';
+import ReactJsPg from './ReactJsPg';
 
 
 function CategoryListModal (properties) {
-    const [reactJsPgListSize, setReactJsPgListSize] = useState(0);
-    const reactJsPgRef = useRef(null);
+  const [reactJsPgListSize, setReactJsPgListSize] = useState(0);
 
-    const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-    const { cleanParam } = useCategoryContext();
-    const [ChildCategoryList, setChildCategoryList] = useState([]);
-    const [selectSearchKey, setSelectSearchKey] = useState('');//select
-    const [inputSearchStr, setInputSearchStr] = useState('');//text
-    
-    const requestHeader = {
-        searchKey : selectSearchKey
-        , searchStr : inputSearchStr
-    };
+  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+  const { cleanParam } = useCategoryContext();
+  const [ChildCategoryList, setChildCategoryList] = useState([]);
+  // const [selectSearchKey, setSelectSearchKey] = useState('');//select
+  // const [inputSearchStr, setInputSearchStr] = useState('');//text
+  const [isRequestHeaderUpdated, setIsRequestHeaderUpdated] = useState(false);
+  const [reactJsPgShow, setReactJsPgShow] = useState(false);
+  //const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const thead=[{key : 'catCd', value : '카테고리 코드'},{key : 'catNm', value : '카테고리 이름'}] ;
-    
-    const getChildCategoryList = async (newRequestHeader, newRequestBody) => {
+  const [selectSearchKey, setSelectSearchKey] = useState('');//select
+  const [inputSearchStr, setInputSearchStr] = useState('');//text
 
-        if(newRequestHeader){
-            requestHeader.searchKey = newRequestHeader.searchKey ? newRequestHeader.searchKey : requestHeader.searchKey;
-            requestHeader.searchStr = newRequestHeader.searchStr ? newRequestHeader.searchStr : requestHeader.searchStr;
-            requestHeader.page = newRequestHeader.page ? newRequestHeader.page : requestHeader.page;
-            requestHeader.size = newRequestHeader.size ? newRequestHeader.size : requestHeader.size;
-        }
+  const [requestHeader, setRequestHeader] = useState({
+    activePage: 1
+    , listSize: 0
+    , size: 10
+    , searchKey : ''
+    , searchStr : ''
+  });
 
-        const rqHeader = cleanParam(requestHeader);
+  const selectOptions = [
+    {key : 'catCd', value : '카테고리 코드'}
+    , {key : 'catNm', value : '카테고리 이름'}
+  ];
 
-        let response;
+  const thead=[{key : 'catCd', value : '카테고리 코드'},{key : 'catNm', value : '카테고리 이름'}] ;
+  
+  const getChildCategoryList = async (requestHeaderParam, requestBody) => {
 
-        try {
-          if (!properties.loadYn){properties.setLoadYn(true);}
-          response = await axios.post(`${apiBaseUrl}/category/getChildCategoryList`, {}, {
-              params : rqHeader
-          });
-          if(response.data != null && response.data != ''){
-              setReactJsPgListSize(response.data[0].totalCnt);
-              
-              if(reactJsPgRef.current){
-                reactJsPgRef.current.updateListSize(response.data[0].totalCnt);
-              }
-            }
-          setChildCategoryList(response.data);
-            
-        } catch (error) {
-            response = error;
-        } finally {
-          properties.setLoadYn(false);
-          return response;
-        }
-    };
-    
-    useEffect(() => {
+    let rqHeader = cleanParam(requestHeaderParam);
+    let response;
 
-        if(properties.isOpen){
-            setSelectSearchKey(properties.searchKey);
-            setInputSearchStr(properties.searchStr);
-            requestHeader.searchKey = properties.searchKey;
-            requestHeader.searchStr = properties.searchStr;
-            search();
-        }
+    try {
+      if (!properties.loadYn){properties.setLoadYn(true);}
+      reactJsPgClear();
+      response = await axios.post(`${apiBaseUrl}/category/getChildCategoryList`, requestBody, {
+          params : rqHeader
+      });
 
-    }, [properties.isOpen, properties.searchKey, properties.searchStr]);
+      let totalCnt = 0;
 
-    if (!properties.isOpen) return null;
+      if(response.data != null && response.data != ''){
+        totalCnt = response.data[0].totalCnt;
+        //setReactJsPgListSize(response.data[0].totalCnt);
+        setChildCategoryList(response.data);
+        reactJsPgSet({...rqHeader, totalCnt:totalCnt, show:true});
 
-    const searchSelectOnChange = function(e){
-        setSelectSearchKey(e.target.value);
-
-        console.log(e.target.value);
-    }
-
-    const searchTextOnChange = function(e){
-        setInputSearchStr(e.target.value);
+      } else {
+        setChildCategoryList([]);
+      }
         
-        console.log(e.target.value);
+    } catch (error) {
+        response = error;
+    } finally {
+      properties.setLoadYn(false);
+      return response;
     }
+  };
+  
+  useEffect(() => {
+    if(properties.isOpen){
+      setSelectSearchKey(properties.searchKey);
+      setInputSearchStr(properties.searchStr);
+      setIsRequestHeaderUpdated(true);
+      
+    } 
+  }, [properties.isOpen, properties.searchKey, properties.searchStr]);
 
-    const search = function(e){
-        getChildCategoryList();
+  useEffect(() => {
+    if(isRequestHeaderUpdated){
+      search({searchKey : properties.searchKey, searchStr : properties.searchStr});
+      setIsRequestHeaderUpdated(false);
     }
+  }, [setSelectSearchKey, setInputSearchStr, isRequestHeaderUpdated]);
 
-    const searchReset = function(e){
-        requestHeader.searchKey = '';
-        requestHeader.searchStr = '';
-        setSelectSearchKey(''); 
-        setInputSearchStr(''); 
-        getChildCategoryList();
+
+  if (!properties.isOpen) return null;
+
+
+  const stateClear = function (){
+    setChildCategoryList([]);
+  }
+
+  const stateSet = (state) => {
+  }
+
+  const inputClear = function(){
+  }
+
+  const reactJsPgClear = function (state) {
+    setReactJsPgListSize(0);
+    setReactJsPgShow(false);
+
+    setRequestHeader({
+      ...requestHeader,
+      activePage : 1,
+      listSize : 0
+    });
+  }
+
+  const reactJsPgSet = function (state) {
+    setReactJsPgListSize(state.totalCnt);
+    setReactJsPgShow(true);
+
+    setRequestHeader({
+      ...requestHeader,
+      activePage : state.activePage,
+      listSize : state.totalCnt
+    });
+  }
+
+  const reactJsPgChange = (pageNumber) => {
+    rowClickCancel();
+
+    const requestHeaderParam = {
+      ...requestHeader,  // 기존 상태 복사
+      activePage: pageNumber,  // 새로운 activePage로 업데이트
+    };
+
+    setRequestHeader({
+      ...requestHeader,
+      activePage: pageNumber
+    });
+
+    getChildCategoryList(requestHeaderParam, {});
+  };
+
+  const search = function(e){
+    
+
+    let requestHeaderParam = {
+      activePage: 1,
+      listSize: 0,
+      size: 10,
+      searchKey: selectSearchKey,
+      searchStr: inputSearchStr
+    };
+    getChildCategoryList(requestHeaderParam, {});
+  }
+
+  const searchReset = function(e){
+    setSelectSearchKey(''); 
+    setInputSearchStr('');
+    // setRequestHeader({
+    //   searchKey : '', 
+    //   searchStr : ''
+    // });
+  }
+
+  const handleSearchKeyChange = (e) => {
+    const newSearchKey = e.target.value;
+    setSelectSearchKey(newSearchKey); 
+    
+    setRequestHeader((prevRequestHeader) => ({
+      ...prevRequestHeader,
+      searchKey: newSearchKey
+    }));
+  }
+
+  const handleSearchStrChange = (e) => {
+    const newSearchStr = e.target.value;
+    setInputSearchStr(newSearchStr);
+
+    setRequestHeader((prevRequestHeader) => ({
+      ...prevRequestHeader,
+      searchStr: newSearchStr
+    }));
+  }
+
+  const onClickSearchResetBtn = function(e){
+    let requestHeaderParam = {
+      activePage: 1,
+      listSize: 0,
+      size: 10
     }
+    searchReset();
+    getChildCategoryList(requestHeaderParam, {});
+  }
 
-    const searchTextOnKeyUp = function(e){
-        if(e.key === 'Enter'){
-            getChildCategoryList()
-        }
+  const inputSearchStrOnKeyUp = function(e){
+    if(e.key == 'Enter'){
+      search();
     }
+  }
 
-    const selectOptions = [{key : 'catNm', value : '카테고리 이름'}, {key : 'catCd', value : '카테고리 코드'}];
+  const rowClickCancel = function () {
+    //ti, i
+    const stateSetParam = {rowClickCatCd : '', rowClickCatSeq : '', catSeq : ''};
+    stateSet(stateSetParam);
+    inputClear();
+  }
 
-    return(
-        <div className='modal category_list_modal'>
-            <div className="modal_child">
-                <div className="header">
-                    <div className="x" onClick={properties.onClose}><img src={icon_x_white}/></div>
-                </div>
-                <div className='search_box_parent'>
-                    <SearchBox options = {selectOptions} searchTextOnChange={searchTextOnChange} searchSelectOnChange={searchSelectOnChange} searchTextOnKeyUp={(e)=>{searchTextOnKeyUp(e)}} search={search} reset={searchReset} searchSelectValue={selectSearchKey} searchTextValue={inputSearchStr} placeholder_str='검색옵션 선택 후 검색어를 입력하세요.' />
-                </div>
-                <BoardList type="DtAttach"  placeholder_str='검색옵션 선택 후 검색어를 입력하세요.' thead={thead} tbody={ChildCategoryList} onRowClick= {properties.onRowClick}/>
-                <div className='pagenation_box'>
-                    <div className='label'>총 카운트 {reactJsPgListSize}</div>
-                    <ReactJsPg ref={reactJsPgRef} getList={getChildCategoryList} requestHeader={requestHeader}/>
-                </div>
-            </div>
-        </div>
-    )
+  return(
+      <div className='modal category_list_modal'>
+          <div className="modal_child">
+              <div className="header">
+                  <div className="x" onClick={properties.onClose}><img src={icon_x_white}/></div>
+              </div>
+              <div className='search_box_parent'>
+                  <SearchBox searchSelectValue={selectSearchKey} searchTextValue={inputSearchStr} searchTextOnChange={(e)=>{handleSearchStrChange(e)}} searchSelectOnChange={(e)=>{handleSearchKeyChange(e)}} searchTextOnKeyUp={(e)=>{inputSearchStrOnKeyUp(e)}} options = {selectOptions} search={search} reset={onClickSearchResetBtn} placeholder_str='검색옵션 선택 후 검색어를 입력하세요.' />
+              </div>
+              <BoardList type="DtAttach"  placeholder_str='검색옵션 선택 후 검색어를 입력하세요.' thead={selectOptions} tbody={ChildCategoryList} onRowClick={properties.onRowClick}/>
+              <div className='pagenation_box'>
+                  <div className='label'>총 카운트 {reactJsPgListSize}</div>
+                  <div className={reactJsPgShow ? 'pg show' : 'pg hide'}>
+                    <ReactJsPg reactJsPgChange={reactJsPgChange} requestHeader={requestHeader}/>
+                  </div>
+                  {/* <ReactJsPg ref={reactJsPgRef} getList={getChildCategoryList} requestHeader={requestHeader}/> */}
+              </div>
+          </div>
+      </div>
+  )
 }
 
 export default CategoryListModal;
-
-
-
-// useEffect(() => {
-//   if (currentPage === start + pageCount) setStart((prev) => prev + pageCount);
-//   if (currentPage < start) setStart((prev) => prev - pageCount);
-// }, [currentPage, pageCount, start]);
-
-class ReactJsPg extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        activePage: 1,
-        listSize : 0,
-        pageSize : 10
-      };
-
-      const totalPages = Math.ceil(this.state.listSize / this.state.pageSize);
-      const start = 1;
-      const noPrev = start == 1;
-      const noNext = start + 4 >= totalPages;
-    }
-
-    updateActivePage = () => {
-      this.setState({activePage : 1});
-    }
-  
-    updateListSize = (newSize) => {
-      this.setState({listSize : newSize});
-    }
-    
-    handlePageChange(pageNumber) {
-      // if (activePage === start + pageCount) setStart((prev) => prev + pageCount);
-      // if (activePage < start) setStart((prev) => prev - pageCount);
-
-      console.log(`Active page is ${pageNumber}`);
-      
-      this.setState({ activePage: pageNumber }, () => {
-        if (this.props.getList) {
-          const updatedHeader = {
-            id : this.props.requestHeader.id
-            , searchKey : this.props.requestHeader.searchKey
-            , searchStr : this.props.requestHeader.searchStr
-            , page: this.state.activePage  // 새로운 activePage로 업데이트
-            , size: this.state.pageSize    // 현재 페이지 크기를 사용
-          };
-    
-          this.props.getList(updatedHeader);
-        }
-      });
-    }
-    
-    render() {
-      return (
-        // <ReactJsPagination
-        //   totalItemsCount={this.state.listSize}//Required
-        //   onChange={this.handlePageChange.bind(this)}//Required
-        //   itemsCountPerPage={this.state.pageSize}
-        //   pageRangeDisplayed={5}
-        //   currentPage={this.state.activePage}
-        // />
-
-        <ReactJsPagination
-          activePage={this.state.activePage}//Required
-          totalItemsCount={this.state.listSize}//Required
-          onChange={this.handlePageChange.bind(this)}//Required
-          itemsCountPerPage={this.state.pageSize}
-          pageRangeDisplayed={5}
-          innerClass='pagination'//ul className
-          itemClass='n5'//li className
-          activeClass='active'//li active className
-          activeLinkClass=''//a active className
-          itemClassFirst='last_item'//<< className
-          itemClassPrev='prev_item'//< className
-          itemClassNext='next_item'//> className
-          itemClassLast='last_item'//>> className
-          hideDisabled={false}
-        />
-      );
-    }
-  }
